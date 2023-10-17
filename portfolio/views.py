ab22bbb8cbed87baa421bd22f1af114d23e4d2a1
Bonolo Kenton Gaosenkwe <bonolo.kamolane@gmail.com>
2023-10-18 01:03:27 +0200

    Initial commit

---
+++ portfolio/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+
+# Create your views here.
+def home(request):
+    return render(request, 'portfolio/index.html')
+
+def contact(request):
+    if request.method == "POST":
+        fullname = request.POST.get('fullname')
+        email = request.POST.get('email')
+        mobile_number = request.POST.get('mobile_number')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        #Send email
+        send_mail(
+            subject,
+            f'Name: {fullname}\nEmail: {email}\nMobile Number: {mobile_number}\nMessage: {message}', 
+            email, ['bonolo.kamolane@gmail.com'],
+        )
+
+        return redirect('home')
+    return render(request, 'portfolio/index.html')
